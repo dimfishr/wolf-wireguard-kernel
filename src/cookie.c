7cb50b4df764e4c6a7086798c1a24fe4f36fd90d
Daniel Pouzzner <douzzer@mega.nu>
2020-10-21 01:08:40 -0500

    Fully functional crypto shim for wolfcrypt in wireguard

--- src/cookie.c
+++ src/cookie.c
@@ -10,8 +10,10 @@
 #include "ratelimiter.h"
 #include "timers.h"
 
+#ifndef WOLFCRYPTO_SHIM_H
 #include <crypto/blake2s.h>
 #include <crypto/chacha20poly1305.h>
+#endif
 
 #include <net/ipv6.h>
 #include <crypto/algapi.h>
